8af5227b811bdaea8ce038bc0d817d85eaa248a2
wen3lee <wen3lee@gmail.com>
2019-05-03 18:11:56 +0800

    advanced version

--- TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe.cpp
@@ -5,119 +5,35 @@ using namespace std;
 
 int main()
 {
-    TicTacToe T;
-    string user1, user2;
-    int pos;
-    int total_loop = SIZE*SIZE-1;
+    TicTacToe gameT;
 
-    int i = 0, k = 0;
-    int count = 0;
+    int loop = SIZE * SIZE;
+    int pos;
     
     cout << "Please enter the first user's (the 'x' user) name: \n";
-    getline(cin, user1);
+    getline(cin, gameT.user1);
     
     
     cout << "Please enter the second user's (the 'o' user) name: \n";
-    getline(cin, user2);
-    
+    getline(cin, gameT.user2);
+
     // loop for input
-    for (i=0; i<total_loop; i++)
+    for (int i=0; i<loop; i++)
     {	
-	    // Prompt the 'x' user to select a grid position where they would like to place an 'x'. 
-		// Prompt the 'o' user to select a grid position where they would like to place an 'o'.
-		if (i % 2 == 0) // first user
-	    {
-		    cout << user1; 
-		}
-		else// (i % 2 == 1) // second user
-		{
-			cout << user2; 
-		}
+    	// select a position
+    	if (0 != gameT.selectAPosition(i)) 
+    	{
+    		// fail selection
+    		return -1;
+    	}
 
-		cout << ", please select a grid position (0-" << total_loop << ")\n" ;	
-	
-	    //check the pos
-		cin >> pos;
-
-		if (T.board[pos / SIZE][pos % SIZE] != '-')
-		{
-			T.printInfo();
-			cout << "The position occupied, please select another grid position (0-15)\n";
-		}
-		else
-		{
-			if (i % 2 == 0) // first user
-		    {
-			    T.board[pos / SIZE][pos % SIZE] = 'x';
-
-			    // win?
-			    // row
-			    count = 0;
-
-			    for (k=0; k<SIZE; k++)
-			    {	
-			    	if ((T.board[pos / SIZE][k]) == 'x')
-			    		count++;  
-				}
-
-				if (count >= SIZE)
-				{
-					cout << user1 << "is the winner!\n";
-					return 0;
-				}	
-
-			    // column
-			    count = 0;
-
-			    for (k=0; k<SIZE; k++)
-			    {	
-			    	if ((T.board[k][pos % SIZE]) == 'x')
-			    		count++;  
-				}
-
-				if (count >= SIZE)
-				{
-					cout << user1 << "is the winner!\n";
-					return 0;
-				}	
-			}
-			else// (i % 2 == 1) // second user
-			{
-				T.board[pos / SIZE][pos % SIZE] = 'o';
-
-				//win?
-				// row
-			    count = 0;
-
-			    for (k=0; k<SIZE; k++)
-			    {	
-			    	if ((T.board[pos / SIZE][k]) == 'o')
-			    		count++;  
-				}
-
-				if (count >= SIZE)
-				{
-					cout << user2 << "is the winner!\n";
-					return 0;
-				}	
-
-			    // column
-			    count = 0;
-
-			    for (k=0; k<SIZE; k++)
-			    {	
-			    	if ((T.board[k][pos % SIZE]) == 'o')
-			    		count++;  
-				}
-
-				if (count >= SIZE)
-				{
-					cout << user2 << "is the winner!\n";
-					return 0;
-				}	
-			}
-		}	
+    	// there is a winner and finish the game
+    	if (0 == gameT.isWinner(i))
+    	{
+    		return 0;
+    	}		
 	}
 
-	cout << "The game is tied!\n" 	   
+	cout << "The game is tied!\n"; 
+	return 0;	   
 }
--- TicTacToe/TicTacToe.hpp
+++ TicTacToe/TicTacToe.hpp
@@ -4,19 +4,32 @@ const int SIZE = 4;
 
 class TicTacToe
 {
-    
+    // selected position (i,j)
+    int position_i;
+    int position_j;
     
 public:
+    // users
+    string user1;
+    string user2;
+
     //Create a 4x4 game board
     char board[SIZE][SIZE];
 
     TicTacToe();
+    int selectAPosition(int num);
+    int isAcceptedPostion();
+    int isWinner(int num);
+    void winnerInfo(int num);
     void printInfo();
 };
 
 //constor
 TicTacToe::TicTacToe()
 {
+    position_i = 0;
+    position_j = 0;
+
     for (int i=0; i<SIZE; i++)
     {
         for (int j=0; j<SIZE; j++)
@@ -25,6 +38,165 @@ TicTacToe::TicTacToe()
         }
     }    
 }
+
+int TicTacToe::selectAPosition(int num)
+{
+    int input_times = 0;
+
+    do
+    {
+        input_times++; 
+
+        // fail selection
+        if(input_times >= 5)
+        {    
+            cout << "Input error is more than five times.\n";
+            return -1;
+        }  
+
+        // Prompt the 'x' user to select a grid position where they would like to place an 'x'. 
+        // Prompt the 'o' user to select a grid position where they would like to place an 'o'.
+        if (num % 2 == 0) // first user
+        {
+            cout << user1; 
+        }
+        else// (i % 2 == 1) // second user
+        {
+            cout << user2; 
+        }
+
+        printf(", please select a position: (ex. 0 0) \n");
+        printInfo();
+
+        cin >> position_i;
+        // consider input fail
+        if(cin.fail())
+        {
+            cout << "Input error! The input should be integers.\n";
+            cin.clear();
+            cin.ignore();
+            position_i = -1;
+            position_j = -1;
+            continue; 
+        }
+
+        cin >> position_j;
+        // consider input fail
+        if(cin.fail())
+        {
+            cout << "Input error! The input should be integers.\n";
+            cin.clear();
+            cin.ignore();
+            position_i = -1;
+            position_j = -1;
+            continue;
+        }
+
+        // debug
+        //printf("(%d, %d)\n", i, j);
+    }while(isAcceptedPostion());
+
+    // selected
+    if (num % 2 == 0) // first user
+    {
+        board[position_i][position_j] = 'x';       
+    }
+    else// (i % 2 == 1) // second user
+    {
+        board[position_i][position_j] = 'o';
+    }    
+
+    return 0;
+}
+
+int TicTacToe::isAcceptedPostion()
+{
+    // out of boundary
+    if (position_i < 0 || position_i >= SIZE || position_j < 0 || position_j >= SIZE)
+    {
+        cout << "The input position is out of boundary!\n";
+        return -1;
+    }
+
+    // occupied   
+    if (board[position_i][position_j] != '-')
+    {
+        cout << "The input position is occupied!\n";
+        return -1;
+    }    
+
+    return 0;
+}
+
+int TicTacToe::isWinner(int num)
+{
+    int count = 0;
+    int i = 0;
+
+    // horizontal
+    for (i=0; i<SIZE; i++)
+    {   
+        if ((board[position_i][i]) == board[position_i][position_j])
+        {    
+            count++;
+        }
+        else
+        {
+            break;
+        }    
+    }
+
+    if (count >= SIZE)
+    {
+        winnerInfo(num);
+     
+        return 0;
+    }   
+
+    // vertical
+    count = 0;
+
+    for (i=0; i<SIZE; i++)
+    {   
+        if ((board[i][position_j]) == board[position_i][position_j])
+        {
+            count++; 
+        }
+        else
+        {
+            break;
+        }         
+    }
+
+    if (count >= SIZE)
+    {
+        winnerInfo(num);
+     
+        return 0;
+    }   
+
+    // diagonally \
+
+    // diagonally /
+
+    return -1;
+}
+
+void TicTacToe::winnerInfo(int num)
+{
+    if (num % 2 == 0) // first user
+    {
+        cout << user1; 
+    }
+    else// (i % 2 == 1) // second user
+    {
+        cout << user2; 
+    }
+
+    cout << " is the winner!\n";
+    printInfo();
+}
+
 
 void TicTacToe::printInfo()
 {
